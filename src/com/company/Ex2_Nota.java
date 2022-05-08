603ac9cb51867c5e693855b715a6b622a0d85039
keidsondesigner <keidsondesigner@gmail.com>
2022-05-07 22:41:16 -0400

    exercicio 3

--- src/com/company/Ex2_Nota.java
+++ src/com/company/Ex2_Nota.java
@@ -8,17 +8,18 @@ Mostre uma mensagem caso o valor seja inválido
 e continue pedindo até que o usuário informe um valor válido.
 */
 public class Ex2_Nota {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 
-        int nota;
+		int nota;
 
-        System.out.println("Nota: ");
-        nota = scan.nextInt();
+		System.out.println("Nota: ");
+		nota = scan.nextInt();
 
-        while (nota < 0 || nota > 10){
-            System.out.println("Nota inválida! Digite novamente: ");
-            nota = scan.nextInt();
-        }
-    }
+		while (nota < 0 || nota > 10){
+			System.out.println("Nota inválida! Digite novamente: ");
+			nota = scan.nextInt();
+		}
+		
+	}
 }
---
+++ src/com/company/Exer3_MaiorEMedia.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que execute 5 vezes,e leia um número por vezes.
+Informe o maior número e a média desses números.
+*/
+
+public class Exer3_MaiorEMedia {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int numero;
+        int maior = 0;
+
+        //int count = 0;
+        /*
+        do {
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+
+            if(numero > maior) maior = numero;
+
+            count = count + 1;
+            System.out.println("O maior número inserido é: " + maior);
+        } while ( count < 5);
+
+        */
+
+        for (int count = 0; count < 5; count++){
+            System.out.println("Número: ");
+            numero = scan.nextInt();
+            if(numero > maior) maior = numero;
+            System.out.println("O maior número inserido é: " + maior);
+        }
+
+        System.out.println("O maior número inserido é: " + maior);
+
+    }
+}
